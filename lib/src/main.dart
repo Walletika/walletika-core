9c6df3340d9bd524edeac806f2b1c4edf2b5a728
MahmoudKhalid <darkness20016@gmail.com>
2022-10-05 20:00:21 +0200

    Auto improt stake contracts

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,16 +1,25 @@
+import 'package:walletika_sdk/src/engine/stake.dart';
+
 import 'core/core.dart';
 
 Future<void> walletikaSDKInitialize() {
   return Future(() {
+    // ABIs loading
     tokenABI;
     stakeABI;
     wnsABI;
 
+    // Database loading
     walletsDB;
     addressesBookDB;
     networksDB;
     tokensDB;
     transactionsDB;
     stakeDB;
+
+    // Stake contracts Fetching
+    importStakeContracts(
+      'https://raw.githubusercontent.com/Walletika/metadata/main/stake-contracts.json',
+    );
   });
 }
