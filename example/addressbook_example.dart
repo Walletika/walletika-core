dbd70e8da8d87aa6d4800849ccdf70594bba25f4
MahmoudKhalid <darkness20016@gmail.com>
2022-10-05 20:03:51 +0200

    Added addressbook examples

---
+++ example/addressbook_example.dart
@@ -0,0 +1,21 @@
+import 'package:walletika_sdk/walletika_sdk.dart';
+import 'package:web3dart/credentials.dart';
+
+void main() async {
+  String username = 'username';
+  EthereumAddress address = EthereumAddress.fromHex(
+    '0xC94EA8D9694cfe25b94D977eEd4D60d7c0985BD3',
+  );
+
+  // initialize walletika SDK
+  await walletikaSDKInitialize();
+
+  // Add new address book
+  bool isAdded = await addNewAddressBook(username: username, address: address);
+
+  // Get all addresses book
+  List<AddressBookModel> allAddressesBook = await getAllAddressesBook();
+
+  // Remove a address book
+  bool isRemoved = await removeAddressBook(allAddressesBook[0]);
+}
