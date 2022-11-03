f777c9ccabefdbe26478163ca71b9cd77bc220a2
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-03 12:15:51 +0200

    Upgrade addresses book

--- lib/src/engine/addressbook.dart
+++ lib/src/engine/addressbook.dart
@@ -4,44 +4,37 @@ import 'package:web3dart/web3dart.dart';
 import '../core/core.dart';
 import '../models.dart';
 
-Future<List<AddressBookModel>> getAllAddressesBook() async {
-  List<AddressBookModel> result = [];
-
-  await for (RowModel row in addressesBookDB.select()) {
-    AddressBookModel addressBook = AddressBookModel.fromJson(row.items);
-    result.add(addressBook);
+Stream<AddressBookModel> getAllAddressesBook() async* {
+  await for (final RowModel row in addressesBookDB.select()) {
+    yield AddressBookModel.fromJson(row.items);
   }
-
-  return result;
 }
 
 Future<bool> addNewAddressBook({
   required String username,
   required EthereumAddress address,
-}) {
-  return Future(() {
-    addressesBookDB.insertSync(
-      rowIndex: addressesBookDB.countRowSync(),
-      items: {
-        "username": username,
-        "address": address.hexEip55,
-      },
-    );
+}) async {
+  await addressesBookDB.insert(
+    rowIndex: addressesBookDB.countRow(),
+    items: {
+      "username": username,
+      "address": address.hexEip55,
+    },
+  );
 
-    addressesBookDB.dumpSync();
+  await addressesBookDB.dump();
 
-    return true;
-  });
+  return true;
 }
 
 Future<bool> removeAddressBook(AddressBookModel addressBook) async {
   bool result = false;
 
-  await for (RowModel row in addressesBookDB.select(
+  await for (final RowModel row in addressesBookDB.select(
     items: addressBook.toJson(),
   )) {
-    addressesBookDB.removeRowSync(row.index);
-    addressesBookDB.dumpSync();
+    addressesBookDB.removeRow(row.index);
+    await addressesBookDB.dump();
     result = true;
     break;
   }
