e0e4dc98d075b3449249897fab8a527dd99d015d
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-19 16:23:53 +0200

    Move ABIs to assets folder

--- lib/src/core/abi_loader.dart
+++ lib/src/core/abi_loader.dart
@@ -1,8 +1,8 @@
 import 'package:path/path.dart' as pathlib;
 
-final String tokenABIPath = pathlib.join('lib', 'src', 'abi', 'token.json');
-final String stakeABIPath = pathlib.join('lib', 'src', 'abi', 'stake.json');
-final String wnsABIPath = pathlib.join('lib', 'src', 'abi', 'wns.json');
+final String tokenABIPath = pathlib.join('assets', 'abi', 'token.json');
+final String stakeABIPath = pathlib.join('assets', 'abi', 'stake.json');
+final String wnsABIPath = pathlib.join('assets', 'abi', 'wns.json');
 
 late String tokenABI;
 late String stakeABI;
